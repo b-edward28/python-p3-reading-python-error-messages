5dad60d81670acc65a34a0a7b181c76cae8a2352
Brendah Edward <edwardbrendah28@gmail.com>
2025-05-09 08:05:22 +0300

    Fix errors

--- lib/a_name_error.py
+++ lib/a_name_error.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python3
-
+hello_world = "Hello World!"
 print(hello_world)
+
+poor_syntax = 2 * 5
+print(poor_syntax)
+
+wrong_type = 'abc' + str(123)
+print(wrong_type)
+
+assert(1 == 1)
+
+
+
